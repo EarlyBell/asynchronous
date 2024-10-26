a257b8000f14f08d647afdd0f7551fba98c9be03
Paul <earlybelljs@gmail.com>
2024-10-26 11:42:00 +0100

    Updated the search feature

--- country.js
+++ country.js
@@ -1,54 +1,108 @@
+const container =
+  document.querySelector(".container");
+const input = document.querySelector("#search");
+const url = "https://restcountries.com/v3.1/all";
 
-// This file fetches country data from an API and dynamically creates HTML elements to display the information.
+const getData = () => {
+container.innerHTML = ' '
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data)
+      data.map((item) => {
+        if (input.value.length < 1) {
+          const country =
+            document.createElement("div");
+          const name =
+            document.createElement("h1");
+          const flag =
+            document.createElement("img");
+          const population =
+            document.createElement("p");
+          const region =
+            document.createElement("p");
+          const capital =
+            document.createElement("p");
 
-// Select the container element where country information will be displayed.
-const container = document.querySelector('.container');
-// Select the search input element.
-const input = document.querySelector('#search');
-// Define the API URL for fetching country data.
-const url = 'https://restcountries.com/v3.1/all';
+          population.innerHTML =
+            "popolation: " +
+            item.population.toLocaleString();
+          region.innerHTML =
+            "region: " + item.region;
+          capital.innerHTML =
+            "capital: " + item.capital;
+          flag.src = item.flags.png;
+          country.classList.add("country");
+          name.innerHTML = item.name.common;
 
-// Define a function 'check' to log the current value of the search input.
-// This function is not currently used but could be implemented for search functionality.
-function check(){
-    console.log(input.value)
-}
+          country.append(flag);
+          country.append(name);
+          country.append(population);
+          country.append(region);
+          country.append(capital);
+          container.appendChild(country);
 
-// Add an 'input' event listener to the search input, calling the 'check' function whenever the input value changes.
-input.addEventListener('input',check);
+          country.addEventListener('click',()=>{
+            const modal = document.createElement('div')
+            const language = document.createElement('p')
+            const currency = document.createElement('p')
+            language.innerHTML = 'language: ' + Object.values(item.languages)
+            modal.classList.add('modal')
+            modal.append(fllation);
+            country.append(region);
+            country.append(capital);
+  
+            country.addEventListener('click',()=>{
+            modal.append(name)
+            modal.append(population)
+            modal.append(region)
+            modal.append(capital)
+            modal.append(language)
+            modal.append(currency)
 
-// Fetch data from the API URL.
-fetch(url)
-// Parse the response as JSON.
-.then((res)=> res.json())
-// Process the fetched country data.
-.then((data) =>{ 
-    // Iterate over each country in the data array.
-    data.map((item)=>{
-        // Create HTML elements for displaying country information.
-        const country = document.createElement('div');
-        const name = document.createElement('h1');
-        const flag = document.createElement('img');
-        const population = document.createElement('p');
-        const region = document.createElement('p');
-        const capital = document.createElement('p');
+            container.append(modal)
+          })
 
-        // Populate the HTML elements with data from the API response.
-        population.innerHTML ='popolation: ' + item.population.toLocaleString();
-        region.innerHTML = 'region: ' + item.region;
-        capital.innerHTML ='capital: ' + item.capital;
-        flag.src = item.flags.png;
-        country.classList.add('country');
-        name.innerHTML = item.name.common;
+          container.append(country);
+        })} else if (
+          item.name.common
+            .toLowerCase()
+            .includes(input.value.toLowerCase())
+        ) {
+          const country =
+            document.createElement("div");
+          const name =
+            document.createElement("h1");
+          const flag =
+            document.createElement("img");
+          const population =
+            document.createElement("p");
+          const region =
+            document.createElement("p");
+          const capital =
+            document.createElement("p");
 
-        // Append the created elements to the country div.
-        country.append(flag);
-        country.append(name);
-        country.append(population);
-        country.append(region);
-        country.append(capital);
+          population.innerHTML =
+            "popolation: " +
+            item.population.toLocaleString();
+          region.innerHTML =
+            "region: " + item.region;
+          capital.innerHTML =
+            "capital: " + item.capital;
+          flag.src = item.flags.png;
+          country.classList.add("country");
+          name.innerHTML = item.name.common;
 
-        // Append the country div to the container.
-        container.append(country);
-    })
-})
+          country.append(flag);
+          country.append(name);
+          country.append(population);
+          country.append(region);
+          country.append(capital);
+
+          container.append(country);
+        }
+      });
+    });
+};
+getData();
+input.addEventListener("input", getData);
